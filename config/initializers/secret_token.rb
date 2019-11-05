0c934441dfba17c3d07e73bd42922c1c51b6e353
StephenJMScott <stephenjmscott@gmail.com>
2019-11-05 16:08:05 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notsosimpleblog::Application.config.secret_token = 'b24c17a9d621152532e7a08a446e9e8105299a0a120b20e0dd3419e5a7833ea1f8c9df57f4f39979a95b2784ecf4412a28eb86b22e073d5d8c4aa4879f930570'
